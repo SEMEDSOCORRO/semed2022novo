85a2a073a56dc8bba2ca87b7adf32661f4435974
Secretária de Educação de Nossa Senhora do Socorro - Sergipe <96733825+SEMEDSOCORRO@users.noreply.github.com>
2022-01-26 12:35:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,10 +6,31 @@
   <title>Matrícula Online 2022 -SEMED</title>
   <!-- Fonte -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+  
   <!-- Estilos -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/form.css">
+  <style>
+		.whatsapp-link {
+			position: fixed;
+			width: 60px;
+			height: 60px;
+			bottom: 40px;
+			right: 40px;
+			background-color: #25d366;
+			color: #fff;
+			border-radius: 50px;
+			text-align: center;
+			font-size: 30px;
+			box-shadow: 1px 1px 2px #888;
+			z-index: 1000;
+		}
+ 
+		.fa-whatsapp {
+			margin-top: 16px;
+		}
+	</style>
   <!-- Scripts (jQuery não pode ser o slim que vem do Boostrap) -->
   <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -32,77 +53,21 @@
       <div id="mainSlider" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="img/matricula22.png" class="d-block w-100" alt="Semed">
-              <a href="https://br.freepik.com/fotos-vetores-gratis/fundo">Fundo vetor criado por macrovector - br.freepik.com</a><br>
-              <div class="carousel-caption d-md-block" style="right: 1.09023% !important; left: 65% !important;">
-                <h5 style="font-family: 'Aleo', serif;">FAÇA AQUI A SUA PRÉ-MATRÍCULA:</h5>
-                <br>
-                <a href="formulario.php" class="main-btn" style="font-family: 'Aleo', serif;"><strong>Pré-Matrícula</strong></a>
+            <img src="img/banner.png" class="d-block w-100" alt="Semed">
+              <div class="carousel-caption d-md-block" style="right: 1.09023% !important; top: 45% !important; ">
+              
+                 <a href="formulario.php" class="main-btn" style="font-family: 'Aleo', serif;"><strong>Fazer Pré-Matrícula</strong></a>
               </div>
             </div>
          </div>
-      <!-- Sobre a Matrícula-->
-      <div id="mainSlider" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/pagina2.png" class="d-block w-100" alt="Semed">
-          <div class="carousel-caption d-md-block"><br>
-            <ul>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Rodapé -->
-  <footer>
-    <div id="contact-area">
-      <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <h3 id="contato" class="main-title">Entre em contato conosco</h3>
-            </div>
-            <div class="col-md-4 contact-box">
-              <i class="fas fa-phone"></i>
-              <p><span class="contact-tile">Ligue para:</span> (79) 2106-7457 / (79) 2106-7453</p>
-              <p><span class="contact-tile">Horários:</span> 8:00 - 14:00</p>
-            </div>
-            <div class="col-md-4 contact-box">
-              <i class="fas fa-envelope"></i>
-              <p><span class="contact-tile">E-mail: </span>matriculas@semedsocorro.com.br</p>
-            </div>
-            <div class="col-md-4 contact-box">
-              <i class="fas fa-map-marker-alt"></i>
-              <p><span class="contact-tile">Venha tomar um café:</span> Rua Padre Manoel Gomes S/N, SN
-                Centro - Nossa Senhora do Socorro - SE CEP 49160-000</p>
-            </div>
-            <div class="col-md-6" id="msg-box">
-              <p>Deixe aqui uma mensagem:</p>
-            </div>
-            <div class="col-md-6" id="contact-form">
-              <form action="" method="POST">
-                <input type="text" class="form-control" placeholder="E-mail" name="email">
-                <input type="text" class="form-control" placeholder="Assunto" name="subject">
-                <textarea class="form-control" rows="3" placeholder="Sua mensagem..." name="message"></textarea>
-                <input class="botao" type="submit" class="enviar"  value="Enviar" />
-              </form>
-            </div>
-          </div>
-      </div>
-    </div><br>
-    <div id="copy-area">
-      <div class="container">
-        <div class="row">
-            <div class="col-md-12"><br>
-              <p> <a href="#"></a> Desenvolvido por SEMED_TIC</a> &copy; 2022</p>
-            </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  
-  <!-- Scripts do projeto -->
-  <script src="js/scripts.js"></script>
-</body>
+               <footer>
+                <div class="col-md-12">
+              <P>Rua Padre Manoel Gomes S/N, SN Centro - Nossa Senhora do Socorro - SE CEP 49160-000</P><p> Desenvolvido por SEMED_NTIC &copy; 2022</p>
+                </div>
+             </footer>
+             <a class="whatsapp-link" href="https://api.whatsapp.com/send?phone=5599111112222" target="_blank"> 
+           <i class="fa fa-whatsapp"></i></a>
+ </body>
 </html>
 
 
